31f5ba902b52d9cfdd7ca54302308c9afc9b20fd
MrElgheim <petter.elgheim@gmail.com>
2022-03-23 15:20:36 +0100

    Update product-schema.js

--- components/product-data/product-schema.js
+++ components/product-data/product-schema.js
@@ -1,33 +1,33 @@
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 
-export const ProductSchema = (product, activeVariantID, site) => {
-  if (!product) return null;
+// export const ProductSchema = (product, activeVariantID, site) => {
+//   if (!product) return null;
 
-  const router = useRouter();
-  const { query } = router;
+//   const router = useRouter();
+//   const { query } = router;
 
-  const variant = product.variants.find((v) => v.id == activeVariantID);
+//   const variant = product.variants.find((v) => v.id == activeVariantID);
 
-  return {
-    "@context": "http://schema.org",
-    "@type": "Product",
-    name: product.title,
-    price: centsToPrice(query.variant ? variant.price : product.price),
-    sku: query.variant ? variant.sku : product.sku,
-    offers: {
-      "@type": "Offer",
-      url: `${site.rootDomain}/products/${product.slug}${
-        query.variant ? `?variant=${variant.id}` : ""
-      }`,
-      availability: query.variant
-        ? `http://schema.org/${variant.inStock ? "InStock" : "SoldOut"}`
-        : `http://schema.org/${product.inStock ? "InStock" : "SoldOut"}`,
-      price: centsToPrice(query.variant ? variant.price : product.price),
-      priceCurrency: "USD",
-    },
-    brand: {
-      "@type": "Brand",
-      name: site.title,
-    },
-  };
-};
+//   return {
+//     "@context": "http://schema.org",
+//     "@type": "Product",
+//     name: product.title,
+//     price: centsToPrice(query.variant ? variant.price : product.price),
+//     sku: query.variant ? variant.sku : product.sku,
+//     offers: {
+//       "@type": "Offer",
+//       url: `${site.rootDomain}/products/${product.slug}${
+//         query.variant ? `?variant=${variant.id}` : ""
+//       }`,
+//       availability: query.variant
+//         ? `http://schema.org/${variant.inStock ? "InStock" : "SoldOut"}`
+//         : `http://schema.org/${product.inStock ? "InStock" : "SoldOut"}`,
+//       price: centsToPrice(query.variant ? variant.price : product.price),
+//       priceCurrency: "USD",
+//     },
+//     brand: {
+//       "@type": "Brand",
+//       name: site.title,
+//     },
+//   };
+// };
